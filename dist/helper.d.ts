a4faf9a7a1c388638e8b2e8e269a1535f2c1b610
Kip Ricker <kip.ricker@gmail.com>
2021-01-13 15:53:28 -0500

    Add dist files

---
+++ dist/helper.d.ts
@@ -0,0 +1,48 @@
+export declare const monthByNumber: {
+    0: string;
+    1: string;
+    2: string;
+    3: string;
+    4: string;
+    5: string;
+    6: string;
+    7: string;
+    8: string;
+    9: string;
+    10: string;
+    11: string;
+};
+export declare const numberByMonth: {
+    January: number;
+    February: number;
+    March: number;
+    April: number;
+    May: number;
+    June: number;
+    July: number;
+    August: number;
+    September: number;
+    October: number;
+    November: number;
+    December: number;
+};
+export declare const daysInMonth: {
+    0: number;
+    1: number;
+    2: number;
+    3: number;
+    4: number;
+    5: number;
+    6: number;
+    7: number;
+    8: number;
+    9: number;
+    10: number;
+    11: number;
+};
+export declare const unit: {
+    day: string;
+    month: string;
+    year: string;
+};
+export declare function getDaysInMonth(selectedMonth: number, selectedYear: number): number;
